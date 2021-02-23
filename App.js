f595c1673bbe7e7bbc91213ac57b4c75c1091d52
Pranay786-gif <79458699+Pranay786-gif@users.noreply.github.com>
2021-02-23 20:25:03 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -11,40 +11,48 @@ import About from './component/About';
 
 function App() {
   return (
-
-    <div className="App">
-      <ReactBootStrap.Navbar bg="light" expand="lg">
-
-        <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
-          <ReactBootStrap.Nav className="mr-auto">
-            <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="/contact">Contect Us</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="/about">About Us</ReactBootStrap.Nav.Link>
-
-
-          </ReactBootStrap.Nav>
-
-        </ReactBootStrap.Navbar.Collapse>
-      </ReactBootStrap.Navbar>
-      <BrowserRouter>
-    <div className= "container">
-    <Switch>
-    <Route exact path= "/" component={Home} />
-    <Route  path= "/contact" component={Contact} />
-    <Route  path= "/about" component={About} />
-    </Switch>
-    </div>
-    </BrowserRouter>
-
-    </div>
+    <div className="background">
     
-  
+    <div className="App">
+    
 
-        );
+    <ReactBootStrap.Navbar bg="primary" expand="lg">
+
+      <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto">
+          <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/contact">Contect Us</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/about">About Us</ReactBootStrap.Nav.Link>
+
+
+        </ReactBootStrap.Nav>
+
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
+    <BrowserRouter>
+  <div className= "container">
+  <Switch>
+  <Route exact path= "/" component={Home} />
+  <Route  path= "/contact" component={Contact} />
+  <Route  path= "/about" component={About} />
+  </Switch>
+  </div>
+  </BrowserRouter>
+  <ReactBootStrap.Image src="img.jpg" width= "300%" height="300%" fluid />
+
+
+  </div>
+
+    </div>
+
+   
+
+
+  );
 
 
 
-        };
+};
 
-        export default App;
+export default App;
